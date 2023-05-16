fee4653c2c4b6c1e03cb22de9ff07fe858f9f2a5
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 23:31:53 +0200

    changed the working dir to include the whole folder structure of Alarm app, still having gitignore though so dont worry :) also tried to debug this mess a bit, but i think i will have to rather work with intent/sharedprefs inside the settings and sync only the alarm itself with a database

---
+++ openCV/build/intermediates/cxx/Debug/2h115o65/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Riegel\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Riegel\\AndroidStudioProjects\\Alarm\\openCV\\.cxx\\Debug\\2h115o65\\armeabi-v7a" ^
+  opencv_jni_shared
