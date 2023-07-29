4c32899c4ee7e7e2ed68232b64508ca7da54080a
Emily <stu227812@mail.uni-kiel.de>
2023-07-29 17:48:39 +0200

    applied new gitignore

--- openCV/build/intermediates/cxx/Debug/2h115o65/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Riegel\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Riegel\\AndroidStudioProjects\\Alarm\\openCV\\.cxx\\Debug\\2h115o65\\armeabi-v7a" ^
-  opencv_jni_shared
--- openCV/build/intermediates/cxx/Debug/2h115o65/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Riegel\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Riegel\\AndroidStudioProjects\\Alarm\\openCV\\.cxx\\Debug\\2h115o65\\x86" ^
-  opencv_jni_shared
