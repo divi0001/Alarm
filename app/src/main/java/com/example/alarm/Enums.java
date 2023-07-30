ddcf53d937f557ad69cb030a90110573b8624dc5
NoneOfYourBussiness <dont@wantto.tell>
2023-07-30 18:48:27 +0200

    started removing the string difficulty/method/submethod to be replaced by enums stored in added Enums.java

---
+++ app/src/main/java/com/example/alarm/Enums.java
@@ -0,0 +1,16 @@
+package com.example.alarm;
+public class Enums {
+
+public enum Difficulties {
+    ExEasy, Easy, Middle, Hard, ExHard, None
+}
+
+public enum SubMethod{
+    Add, Sub, Mult, Div, Fac, Root, FuncVal, Extrema, MultipleChoice, Leave, Enter, None
+}
+
+public enum Method{
+    TapOff, Math, QRBar, Location, Sudoku, Memory, Passphrase, None
+}
+
+}
