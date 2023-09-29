66a2662686dbe8446eb5eb08f0354ab763b07b26
alsoNoneOfYourBussiness <whywouldyou@care.com>
2023-09-29 19:29:49 +0200

    deleted kotlin package in main branch

--- app/src/main/java/com/example/alarm/sudokupkgkotlin/game/Cell.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alarm.sudokupkgkotlin.game
-
-class Cell(
-    val row: Int,
-    val col: Int,
-    var value: Int,
-    var isStartingCell: Boolean = false,
-    var notes: MutableSet<Int> = mutableSetOf<Int>()
-)
--- app/src/main/java/com/example/alarm/sudokupkgkotlin/viewmodel/PlaySudokuViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.alarm.sudokupkgkotlin.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.alarm.sudokupkgkotlin.game.SudokuGame
-
-class PlaySudokuViewModel : ViewModel() {
-    val sudokuGame = SudokuGame()
-}
